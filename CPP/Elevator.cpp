2b102fbba43590f9c1fd18e2917ffa1a47fa44a9
Sasaki42 <tylersasaki42@gmail.com>
2016-02-04 20:41:26 -0800

    Elevator code:  Updated comments and did minor code clean-up
    DKE:  I cleaned up the comments for the elevator logic.  I also cleaned
    up some of the method and variable names left over from last year that
    weren't really applicable to this year's elevator logic.

--- CPP/Elevator.cpp
+++ CPP/Elevator.cpp
@@ -1,5 +1,15 @@
 #include "../H/Elevator.h"
 
+//------------------------------------------------------------------------------
+// METHOD:  Elevator::Elevator()
+// Type:	Public constructor method
+//------------------------------------------------------------------------------
+// Creates an elevator object using as input:
+// - Elevator motor PWM channel
+// - Elevator potentiometer analog channel
+// - Upper limit switch digital I/O channel
+// - Lower limit switch digital I/O channel
+//------------------------------------------------------------------------------
 Elevator::Elevator(uint elevMotorCh, uint elevPotCh, uint upperLimitSwCh, uint lowerLimitSwCh)
 {
 	pElevatorMotor    = new Talon(elevMotorCh);
@@ -15,6 +25,12 @@ Elevator::Elevator(uint elevMotorCh, uint elevPotCh, uint upperLimitSwCh, uint l
 	MoveElevator(pElevatorPot->Get());
 }
 
+//------------------------------------------------------------------------------
+// METHOD:  Elevator::Elevator()
+// Type:	Public destructor method
+//------------------------------------------------------------------------------
+// Destroys the elevator object
+//------------------------------------------------------------------------------
 Elevator::~Elevator()
 {
 }
@@ -22,21 +38,18 @@ Elevator::~Elevator()
 // METHOD:  Elevator::MoveElevator()
 // Type:	Public accessor method
 //------------------------------------------------------------------------------
-// Calculates a target robot POT value based on input base and offset
-// positions.
+// Calculates a target robot POT value based on input target position
+// and then moves the elevator to the desired position.
 //------------------------------------------------------------------------------
 bool  Elevator::MoveElevator(uint inputTarget)
 {
 	bool   targetFound  = false;
-	double baseTarget   = 0;
 
 	targetInput    = inputTarget;
 
- 	baseTarget     = CalcBaseTarget(inputTarget);
+	elevatorTarget = CalcPOTTarget(inputTarget);
 
-	elevatorTarget = baseTarget;
-
-	targetFound = GoToPotTargetNoPID(elevatorTarget);
+	targetFound = GoToPotTarget(elevatorTarget);
 
 	return targetFound;
 }
@@ -54,7 +67,7 @@ float  Elevator::GetTargetMotorSpeed() const
 // METHOD:  Elevator::GetMotorSpeed()
 // Type:	Public accessor method
 //------------------------------------------------------------------------------
-// Returns the current elevator motor speed.
+// Returns the current actual elevator motor speed.
 //------------------------------------------------------------------------------
 float  Elevator::GetMotorSpeed() const
 {
@@ -64,7 +77,7 @@ float  Elevator::GetMotorSpeed() const
 // METHOD:  Elevator::GetCurrentPosition()
 // Type:	Public accessor method
 //------------------------------------------------------------------------------
-// Returns the current elevator position potentiometer reading.
+// Returns the current actual elevator position potentiometer reading.
 //------------------------------------------------------------------------------
 double Elevator::GetCurrentPosition() const
 {
@@ -85,7 +98,7 @@ double Elevator::GetPositionTarget() const
 // METHOD:  Elevator::GetPositionTargetInput()
 // Type:	Public accessor method
 //------------------------------------------------------------------------------
-// Returns the target elevator input potentiometer reading.
+// Returns the target elevator position input value.
 //------------------------------------------------------------------------------
 uint   Elevator::GetPositionTargetInput() const
 {
@@ -98,6 +111,8 @@ uint   Elevator::GetPositionTargetInput() const
 //------------------------------------------------------------------------------
 // Returns the value of the upper limit switch.  Note the limit switches read
 // the opposite of what we want them to read to be intuitive.
+// *** WE WANT TO CHANGE THE WIRING ON THE LIMIT SWITCHES TO READ THE OPPOSITE
+// *** OF WHAT THEY DO NOW SO THE CODE WILL BE MORE INTUITIVE
 //------------------------------------------------------------------------------
 bool   Elevator::GetUpperLimitSwitch() const
 {
@@ -115,7 +130,9 @@ bool   Elevator::GetUpperLimitSwitch() const
 // Type:	Public accessor method
 //------------------------------------------------------------------------------
 // Returns the value of the lower limit switch.  Note the limit switches read
-//  the opposite of what we want them to read to be intuitive.
+// the opposite of what we want them to read to be intuitive.
+// *** WE WANT TO CHANGE THE WIRING ON THE LIMIT SWITCHES TO READ THE OPPOSITE
+// *** OF WHAT THEY DO NOW SO THE CODE WILL BE MORE INTUITIVE
 //------------------------------------------------------------------------------
 bool   Elevator::GetLowerLimitSwitch() const
 {
@@ -129,16 +146,17 @@ bool   Elevator::GetLowerLimitSwitch() const
 	return lowerLimit;
 }
 //------------------------------------------------------------------------------
-// METHOD:  Elevator::CalcBaseTarget()
+// METHOD:  Elevator::CalcPOTTarget()
 // Type:	Private method
 //------------------------------------------------------------------------------
-// Determines the base elevator position target based on an input position.
+// Determines the elevator potentiometer target based on a passed target input
+// position.
 //------------------------------------------------------------------------------
-double Elevator::CalcBaseTarget(uint basePosition)
+double Elevator::CalcPOTTarget(uint targetPosition)
 {
 	double targetPot = 0;
 
-    switch ( basePosition )
+    switch ( targetPosition )
     {
     	case kGround:
 			targetPot = ELEV_POS_LOWER_LIMIT;
@@ -157,12 +175,17 @@ double Elevator::CalcBaseTarget(uint basePosition)
 }
 
 //------------------------------------------------------------------------------
-// METHOD:  Elevator::GoToPotTargetNoPID()
+// METHOD:  Elevator::GoToPotTarget()
 // Type:	Private method
 //------------------------------------------------------------------------------
-//
+// Moves the arm elevator until it reaches the target position, or activates
+// the upper or lower limit switches.
+// *** NOTE THE LIMIT SWITCHES CURRENTLY READ THE OPPOSITION OF WHAT IS
+// *** INTUITIVE.  WE WANT TO CHANGE THE WIRING SO THE LIMIT SWITCHES WILL
+// *** READ FALSE WHEN NOT ACTIVATED AND TRUE WHEN ACTIVATED SO THE CODE CAN
+// *** BE MORE INTUITIVE.
 //------------------------------------------------------------------------------
-bool  Elevator::GoToPotTargetNoPID(double inputPotValue)
+bool  Elevator::GoToPotTarget(double inputPotValue)
 {
 	bool   potTargetFound  = false;
 	double targetLowValue  = inputPotValue - TARGET_TOLERANCE;
--- H/Elevator.h
+++ H/Elevator.h
@@ -6,10 +6,8 @@
 //------------------------------------------------------------------------------
 // DEFINE Elevator CLASS
 //------------------------------------------------------------------------------
-// Positions the elevator based on a target potentiometer reading.  Class
-// has two primary input methods:
-// 1) Driver station potentiometer reading
-// 2) Driver station switch values for base position plus an off-set
+// Positions the elevator based on a target position.  The target position
+// translates into a target potentiometer reading.
 //------------------------------------------------------------------------------
 class Elevator
 {
@@ -30,9 +28,9 @@ class Elevator
 		bool   GetLowerLimitSwitch() const;
 
 	private:
-		const float   MOTOR_SPEED_UP            =   0.5;    // CONFIGURE
-		const float   MOTOR_SPEED_DOWN          =  -0.5;    // CONFIGURE
-		const float   ALL_STOP                  =   0.0;
+		const float   MOTOR_SPEED_UP            =   0.25;   // CONFIGURE
+		const float   MOTOR_SPEED_DOWN          =  -0.25;   // CONFIGURE
+		const float   ALL_STOP                  =   0.00;
 
 		const double  ELEV_POS_UPPER_LIMIT      =  75.0;    // CONFIGURE
 		const double  ELEV_POS_LOWER_LIMIT      =  25.0;    // CONFIGURE
@@ -42,9 +40,8 @@ class Elevator
 		const double  POT_OFFSET                =   0.0;    // CONFIGURE
 
 		double CalcTargetPotValue(double inputPotValue);
- 		double CalcBaseTarget(uint basePosition);
- 		bool   GoToPotTargetNoPID(double potTarget);
-		bool   GoToPotTargetPID(double potTarget);
+ 		double CalcPOTTarget(uint targetPosition);
+ 		bool   GoToPotTarget(double potTarget);
 
 		Talon               *pElevatorMotor;
 		AnalogPotentiometer *pElevatorPot;
