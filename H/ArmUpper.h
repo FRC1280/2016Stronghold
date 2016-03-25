5aa6cd02ad0e31452603b0b2551afe2406890919
Sasaki42 <tylersasaki42@gmail.com>
2016-03-24 18:42:46 -0700

    Further Tuning of Robot POTs & Consolidate to CCI1
    Tuned the robot POT readings some more.
    Consolidated the driver station switches to CCI1 after the team broke
    CCI2.  Eliminated switches not yet supported in code.

--- H/ArmUpper.h
+++ H/ArmUpper.h
@@ -33,18 +33,18 @@ class ArmUpper
 		double GetConstant()         const;
 
 	private:
-		const float   MOTOR_SPEED_UP           =    1.00;
-		const float   MOTOR_SPEED_DOWN         =   -1.00;
+		const float   MOTOR_SPEED_UP           =   -1.00;
+		const float   MOTOR_SPEED_DOWN         =    1.00;
 		const float   ALL_STOP                 =    0.00;
 
 		const double  INPUT_POT_FULL_FWD       =   -1.000;   // CONFIGURE
 		const double  INPUT_POT_FULL_BACK      =    1.000;   // CONFIGURE
 
-		const double  POT_FULL_RANGE           = -3600.0;    // CONFIGURE
-		const double  POT_OFFSET               =  3081.6;    // CONFIGURE
+		const double  POT_FULL_RANGE           =  3600.0;    // CONFIGURE
+		const double  POT_OFFSET               = -1797.1;    // CONFIGURE
 
-		const double  OUTPUT_POT_FULL_FWD      =    0.000;   // CONFIGURE
-		const double  OUTPUT_POT_FULL_BACK     =  270.000;   // CONFIGURE
+		const double  OUTPUT_POT_FULL_FWD      =    5.000;   // CONFIGURE
+		const double  OUTPUT_POT_FULL_BACK     =  180.000;   // CONFIGURE
 
 		const double  ARM_TOP                  =    25.0;    // CONFIGURE
 		const double  ARM_MIDDLE               =    50.0;    // CONFIGURE
