ab6425052d66586ca0ba73a04388d4ab639ee964
Sasaki42 <tylersasaki42@gmail.com>
2016-04-07 18:21:30 -0700

    Autonomous Mode Tuning

--- src/MainRobotCode.cpp
+++ src/MainRobotCode.cpp
@@ -146,23 +146,23 @@ class StrongholdRobot : public IterativeRobot
         // Autonomous mode timings (Starts and Durations)
 		// For durations, 50 loops is approximately 1 second.
 		static const uint  AM2_S1_START            = 100;  // CONFIGURE
-		static const uint  AM2_S1_DRIVE_FWD        = 275;  // CONFIGURE
+		static const uint  AM2_S1_DRIVE_FWD        = 255;  // CONFIGURE
 
 		static const uint  AM3_S1_START            =   0;  // CONFIGURE
-		static const uint  AM3_S1_DRIVE_FWD        = 275;  // CONFIGURE
+		static const uint  AM3_S1_DRIVE_FWD        = 255;  // CONFIGURE
 
 		static const uint  AM4_S1_START            = 100;  // CONFIGURE
-		static const uint  AM4_S1_DRIVE_FWD        = 275;  // CONFIGURE
+		static const uint  AM4_S1_DRIVE_FWD        = 225;  // CONFIGURE
 		static const uint  AM4_S2_TURN_AROUND      =  50;  // CONFIGURE
-		static const uint  AM4_S3_DRIVE_FWD        = 275;  // CONFIGURE
+		static const uint  AM4_S3_DRIVE_FWD        = 225;  // CONFIGURE
 
 		static const uint  AM5_S1_START            = 100;  // CONFIGURE
-		static const uint  AM5_S1_DRIVE            = 275;  // CONFIGURE
+		static const uint  AM5_S1_DRIVE            = 225;  // CONFIGURE
 		static const uint  AM5_S2_TURN_RT          = 100;  // CONFIGURE
-		static const uint  AM5_S3_EJECT            = 100;  // CONFIGURE
+		static const uint  AM5_S3_EJECT            = 150;  // CONFIGURE
 
 		static const uint  AM6_S1_START            = 100;  // CONFIGURE
-		static const uint  AM6_S1_DRIVE            = 275;  // CONFIGURE
+		static const uint  AM6_S1_DRIVE            = 225;  // CONFIGURE
 		static const uint  AM6_S2_TURN_RT          =  25;  // CONFIGURE
 		static const uint  AM6_S3_SHOOT            = 250;  // CONFIGURE
 
